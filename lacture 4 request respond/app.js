42039e29bcc73f413e721bf35180368be5f2048d
Gautam99-coder <gautamtharu1000@gmail.com>
2025-08-10 21:45:06 +0530

    url respond

--- lacture 4 request respond/app.js
+++ lacture 4 request respond/app.js
@@ -2,13 +2,22 @@
 const http=require('http');
 const server=http.createServer((req,res)=>{
     console.log(req.url,req.method,req.headers);
-    res.setHeader('Content-Type','Text/html');
+    if(req.url==='/'){
+    res.setHeader('Content-Type','text/html');
     res.write('<html>');
     res.write('<head><title>Complete Coding</title></head>');
     res.write('</html>');
-    res.write('<body><h1>Welcome to Complete Coding</h1></body>');
-    res.end();
-    
+    res.write('<body><h1>Welcome to Home</h1></body>');
+    return res.end();
+    }
+    else if(req.url==='/product'){
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>Complete Coding</title></head>');
+    res.write('</html>');
+    res.write('<body><h1>Checkout our products</h1></body>');
+    return res.end();
+    }
 });
 const PORT=3001;
 server.listen(PORT,()=>{
