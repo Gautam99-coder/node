578894f3530ed79f64f3769a5f4ae0051025163c
Gautam99-coder <gautamtharu1000@gmail.com>
2025-08-12 22:05:25 +0530

    parsing request

--- lacture 5/user.js
+++ lacture 5/user.js
@@ -1,6 +1,7 @@
 //form
 const http = require('http');
 const fs = require('fs');
+const { json } = require('stream/consumers');
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
     if (req.url === '/') {
@@ -11,6 +12,10 @@ const server = http.createServer((req, res) => {
         res.write('<body>');
         res.write('<form action="/submit" method="POST">');
         res.write('<input type="text" name="username" placeholder="Enter your name">');
+        res.write('<label for="male">Male</label>');
+        res.write('<input type="radio" name="gender" value="male" id=male>');
+        res.write('<label for="female">Female</label>');
+        res.write('<input type="radio" name="gender" value="female" id=female>');
         res.write('<button type="submit">Submit</button>');
         res.write('</form>');
         res.write('</body>');
@@ -22,25 +27,15 @@ const server = http.createServer((req, res) => {
             body.push(chunk);
         });
         req.on('end', () => {
-            console.log('Received data:', body);
-            res.setHeader('Content-Type', 'text/html');
-            res.write('<html>');
-            res.write('<head><title>Form Submitted</title></head>');
-            res.write('</html>');
-            res.write('<body>');
-            res.write('<h1>Form submitted successfully!</h1>');
-            res.write(`<p>${body}</p>`);
-            res.write('</body>');
             console.log('Form data:', Buffer.concat(body).toString());
             // Here you can process the form data as needed
-            res.write('<a href="/">Go back</a>');
-            res.write('</body>');
             const params=new URLSearchParams(Buffer.concat(body).toString());
             const bodyObject={};
             for(const [key,val] of params.entries()){
                 bodyObject[key]=val;
             }
             console.log(bodyObject);
+            fs.writeFileSync('user.txt', JSON.stringify(bodyObject));
             return res.end();
         });
     } else {
