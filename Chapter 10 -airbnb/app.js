816e62a29b26743bc3cd2e0534fcba2498996f8a
Gautam99-coder <gautamtharu1000@gmail.com>
2025-08-29 20:57:28 +0530

    create status 404 , pag is not working

--- Chapter 10 -airbnb/app.js
+++ Chapter 10 -airbnb/app.js
@@ -16,6 +16,9 @@ app.use((req,res,next)=>{
 app.use("/",userRouter);
 app.use(express.urlencoded()); //to parse the incoming form data in req.body
 app.use(hostRouter);
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>404 Page Not Found</h1>');
+});
 const PORT=3001;
 app.listen(PORT,()=>{
     console.log(`Server running on address http://localhost${PORT}`)
